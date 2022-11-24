8bd6e65b0f39a009186dc25e9be3913fa2f02dad
Deniz Atas <denizatas.18@gmail.com>
2022-11-24 22:39:43 +0300

    add searchbar

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { Button, Form, Card } from 'react-bootstrap';
 
 const WeatherForecast = () => {
 
@@ -25,24 +26,35 @@ const WeatherForecast = () => {
     }
 
     return (
-        <div>
-            {
-                data ? (
-                    data.list.map((dt, index) => (
-                        <ul key={index}>
-                            <li>{dt.dt_txt}</li>
-                            <li>{dt.main.temp}</li>
-                            <li>{dt.weather[0].main}</li>
-                            <li>{dt.visibility}</li>
-                            <li>{dt.main.feels_like}</li>
-                            <li>{dt.main.humidity}</li>
-                            <li>{dt.wind.speed}</li>
-                        </ul>
-                    ))
-                ) : null
-                
-            }
-        </div>
+        <>
+            <div>
+                <Card className="mt-2" style={{width: '25rem', margin: '0 auto', background: 'linear-gradient(to right, rgba(102, 126, 234, 0.5), rgba(118, 75, 162, 0.5))'}}>
+                    <Form className="mb-5 mt-5 opacity-25 col-11" style={{margin: '0 auto'}}>
+                        <Form.Group>
+                            <Form.Control type="text" placeholder="Country"></Form.Control>
+                        </Form.Group>
+                    </Form>
+                    {
+                        data ? (
+                            data.list.map((dt, index) => (
+                                <ul key={index}>
+                                    <li>{dt.dt_txt}</li>
+                                    <li>{dt.main.temp}</li>
+                                    <li>{dt.weather[0].main}</li>
+                                    <li>{dt.visibility}</li>
+                                    <li>{dt.main.feels_like}</li>
+                                    <li>{dt.main.humidity}</li>
+                                    <li>{dt.wind.speed}</li>
+                                </ul>
+                            ))
+                        ) : null
+
+                    }
+                </Card>
+            </div>
+            <Button className="btn" >React Button</Button>
+        </>
+
     );
 }
 export default WeatherForecast;
