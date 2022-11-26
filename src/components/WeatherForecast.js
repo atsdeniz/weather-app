eca8093a4269b1dc61037acbd97f631d25330351
Deniz Atas <denizatas.18@gmail.com>
2022-11-26 11:47:08 +0300

    redesign component

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import { Button, Form, Card } from 'react-bootstrap';
+import { BsSearch } from "react-icons/bs";
+import { WiRain, WiCloudy, WiDaySunny } from "react-icons/wi";
 
 const WeatherForecast = () => {
 
@@ -8,6 +10,35 @@ const WeatherForecast = () => {
 
     const [location, setLocation] = useState('Istanbul');
     const [data, setData] = useState('');
+    const [searchValue, setSearchValue] = useState('');
+
+
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLocation(searchValue);
+    }
+
+    const cleanSearch = () => setSearchValue('');
+
+    let wicon;
+    switch(data ? data.list[0].weather[0].main : ""){
+        case "Rain":
+            wicon = <WiRain  style={{color: 'white', fontSize: '200px'}} />;
+            break;
+        case "Clouds":
+            wicon = <WiCloudy  style={{color: 'white', fontSize: '200px'}}/>;
+            break;
+        case "Sunny":
+            wicon = <WiDaySunny  style={{color: 'yellow', fontSize: '200px'}}/>;
+            break;
+        default:
+            wicon = "";
+
+    }
 
 
     useEffect(() => {
@@ -21,38 +52,38 @@ const WeatherForecast = () => {
             })
     }, [location]);
 
-    if (data) {
-        console.log("data: ", data.city.name);
-    }
 
     return (
         <>
             <div>
-                <Card className="mt-2" style={{width: '25rem', margin: '0 auto', background: 'linear-gradient(to right, rgba(102, 126, 234, 0.5), rgba(118, 75, 162, 0.5))'}}>
-                    <Form className="mb-5 mt-5 opacity-25 col-11" style={{margin: '0 auto'}}>
+                <Card className="mt-2" style={{ width: '25rem', height: '37rem', margin: '0 auto', background: 'linear-gradient(to right, rgba(102, 126, 234, 0.5), rgba(118, 75, 162, 0.5))' }}>
+                    <Form onSubmit={handleSubmit} className="mb-5 mt-5 opacity-25 col-11" style={{ margin: '0 auto' }}>
                         <Form.Group>
-                            <Form.Control type="text" placeholder="Country"></Form.Control>
+                            <div className="wrapper">
+                                <Form.Control className="input"
+                                    type="text"
+                                    placeholder="Search"
+                                    value={searchValue}
+                                    onChange={handleChange}
+                                    onClick={cleanSearch} >
+                                </Form.Control>
+                                <span className="icon"><BsSearch /></span>
+                            </div>
                         </Form.Group>
                     </Form>
                     {
                         data ? (
-                            data.list.map((dt, index) => (
-                                <ul key={index}>
-                                    <li>{dt.dt_txt}</li>
-                                    <li>{dt.main.temp}</li>
-                                    <li>{dt.weather[0].main}</li>
-                                    <li>{dt.visibility}</li>
-                                    <li>{dt.main.feels_like}</li>
-                                    <li>{dt.main.humidity}</li>
-                                    <li>{dt.wind.speed}</li>
-                                </ul>
-                            ))
+                            <div>
+                                {wicon}
+                                <h2>{data.city.name}</h2>
+                                <h5>{new Date().toLocaleString() + ''}</h5>
+                                <h1>{Math.ceil(data.list[0].main.temp - 273.15) + '\u00b0'}</h1>
+                            </div>
                         ) : null
 
                     }
                 </Card>
             </div>
-            <Button className="btn" >React Button</Button>
         </>
 
     );
